0950dc08e1426e6954b652a826e3f9cb0a6ab538
Olivia <qwert45640@gmail.com>
2026-02-25 16:45:39 +0900

    AJAX 방식의 센서 모니터링

--- api/app.py
+++ api/app.py
@@ -158,3 +158,10 @@ def create_app(test_config=None):
        })
 
    return app
+
+    @app.route('/user/<int:user_id>', methods=['GET'])
+    def get_user_info(user_id):
+    user = get_user(user_id)
+    if user is None:
+        return '사용자가 존재하지 않습니다.', 404
+    return jsonify(user)
